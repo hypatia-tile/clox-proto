617ab837aa9794993fd3a1069558ccd7a082cbf5
shinokun <hypatia.tile02021@gmail.com>
2026-04-13 19:47:44 +0900

    feat: add dynamic array and memory management

--- ex01/src/main.c
+++ ex01/src/main.c
@@ -1,6 +1,13 @@
 #include "common.h"
+#include "chunk.h"
 
 int main(void) {
-  printf("Hello, World!\n");
+  Chunk chunk;
+  initChunk(&chunk);
+  writeChunk(&chunk, OP_RETURN);
+  for (size_t i = 0; i < sizeof(chunk.code); i++) {
+    printf("%hhu", (chunk.code)[i]);
+  }
+  freeChunk(&chunk);
   return 0;
 }
