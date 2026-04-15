8e0936cbc552d7ad8333dae6d60c87a3b1d03d9a
shinokun <hypatia.tile02021@grail.com>
2026-04-15 23:06:16 +0900

    feat: add line number to debug output

--- ex01/include/chunk.h
+++ ex01/include/chunk.h
@@ -12,13 +12,14 @@ typedef enum {
 typedef struct {
   int count;
   int capacity;
-  uint8_t* code;
+  uint8_t *code;
+  int *lines;
   ValueArray constants;
 } Chunk;
 
-void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
-void freeChunk(Chunk* chunk);
-int addConstant(Chunk* chunk, Value value);
+void initChunk(Chunk *chunk);
+void writeChunk(Chunk *chunk, uint8_t byte, int line);
+void freeChunk(Chunk *chunk);
+int addConstant(Chunk *chunk, Value value);
 
 #endif // clox_chunk_h
