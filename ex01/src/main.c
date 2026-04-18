352303085f10d42d5bddc1b881ec35a39a1c3612
shinokun <hypatia.tile02021@gmail.com>
2026-04-18 16:13:07 +0900

    feat 15.1 - equip virtual machine add a viraual machine with minimal skeleton feature.

--- ex01/src/main.c
+++ ex01/src/main.c
@@ -1,7 +1,9 @@
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(void) {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
 
@@ -10,6 +12,8 @@ int main(void) {
   writeChunk(&chunk, constant, 123);
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }
