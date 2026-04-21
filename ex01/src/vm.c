4e8cd70f7075a83d5310eef3dcc09e1fbabad938
shinokun <hypatia.tile02021@gmail.com>
2026-04-21 23:16:57 +0900

    feat 16.1.1-16.1.2:
    Add compiler and scanner modules

--- ex01/src/vm.c
+++ ex01/src/vm.c
@@ -1,5 +1,6 @@
 #include "vm.h"
 #include "chunk.h"
+#include "compiler.h"
 #include "debug.h"
 #include "stdio.h"
 #include "stdbool.h"
@@ -69,8 +70,7 @@ static InterpretResult run() {
 #undef READ_CONSTANT
 }
 
-InterpretResult interpret(Chunk *chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char *source) {
+  compile(source);
+  return INTERPRET_OK;
 }
