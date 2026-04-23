3d2714589126f743dabe158ccc49388603c586b2
shinokun <hypatia.tile02021@gmail.com>
2026-04-23 19:32:16 +0900

    format

--- ex01/src/scanner.c
+++ ex01/src/scanner.c
@@ -138,9 +138,11 @@ static TokenType identifierType() {
     return checkKeyword(1, 4, "uper", TOKEN_SUPER);
   case 't':
     if (scanner.current - scanner.start > 1) {
-      switch(scanner.start[1]) {
-        case 'h': return checkKeyword(2, 2, "is", TOKEN_THIS);
-        case 'r': return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+      switch (scanner.start[1]) {
+      case 'h':
+        return checkKeyword(2, 2, "is", TOKEN_THIS);
+      case 'r':
+        return checkKeyword(2, 2, "ue", TOKEN_TRUE);
       }
     }
     break;
