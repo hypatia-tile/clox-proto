96bde98b8978ab97fcb1eab3b7c0989ec38a84f1
shinokun <hypatia.tile02021@gmail.com>
2026-04-25 13:36:27 +0900

    feat: 17.4.2
    recognize number token and translate it into corresponding bytecode.

--- ex01/src/compiler.c
+++ ex01/src/compiler.c
@@ -1,5 +1,8 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "chunk.h"
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
@@ -7,9 +10,39 @@
 typedef struct {
   Token current;
   Token previous;
+  bool hadError;
+  bool panicMode;
 } Parser;
 
 Parser parser;
+
+Chunk *compilingChunk;
+
+static Chunk *currentChunk() {
+  return compilingChunk;
+}
+
+static void errorAt(Token *token, const char *message) {
+  if (parser.panicMode) return;
+  parser.panicMode = true;
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == TOKEN_ERROR) {
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // Nothing.
+  } else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+static void error(const char *message) { errorAt(&parser.previous, message); }
+
+static void errorAtCurrent(const char *message) {
+  errorAt(&parser.current, message);
+}
 
 static void advance() {
   parser.previous = parser.current;
@@ -22,8 +55,64 @@ static void advance() {
   }
 }
 
+static void consume(TokenType type, const char *message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() {
+  emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value) {
+  int constant= addConstant(currentChunk(), value);
+  if (constant > UINT8_MAX) {
+    error("Too many constants in one chunk.");
+    return 0;
+  }
+
+  return (uint8_t)constant;
+}
+
+static void emitConstant(Value value) {
+  emitBytes(OP_SUBTRACT, makeConstant(value));
+}
+
+static void endCompiler() {
+  emitReturn();
+}
+
+static void number() {
+  double value = strtod(parser.previous.start, NULL);
+  emitConstant(value);
+}
+
+static void expression() {
+  // What goes here?
+}
+
 bool compile(const char *source, Chunk *chunk) {
   initScanner(source);
+  compilingChunk = chunk;
+
+  parser.hadError = false;
+  parser.panicMode = false;
+
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
+  return !parser.hadError;
 }
