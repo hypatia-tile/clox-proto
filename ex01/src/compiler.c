98c43708c3167c461fc06dd4c61d399472cd605a
shinokun <hypatia.tile02021@gmail.com>
2026-06-20 10:04:18 +0900

    feat: 22.2

--- ex01/src/compiler.c
+++ ex01/src/compiler.c
@@ -166,6 +166,14 @@ static void endCompiler() {
 #endif // DEBUG_PRINT_CODE
 }
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
 static void unary(bool canAssign) {
   (void) canAssign;
   TokenType operatorType = parser.previous.type;
@@ -323,6 +331,14 @@ static void literal(bool canAssign) {
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration() {
   uint8_t global = parseVariable("Expect variable name.");
   if (match(TOKEN_EQUAL)) {
@@ -382,6 +398,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
