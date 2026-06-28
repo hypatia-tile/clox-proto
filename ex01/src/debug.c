4c51f8b561955e6cb2da6e2fe4e453f992942bf9
shinokun <hypatia.tile02021@gmail.com>
2026-06-28 12:53:23 +0900

    format: cast with no space

--- ex01/src/debug.c
+++ ex01/src/debug.c
@@ -29,9 +29,10 @@ static int byteInstruction(const char *name, Chunk *chunk, int offset) {
   return offset + 2;
 }
 
-static int jumpInstruction(const char *name, int sign, Chunk *chunk, int offset) {
-  uint16_t jump = (uint16_t) (chunk->code[offset + 1] << 8);
-  jump |= (uint16_t) chunk->code[offset+2];
+static int jumpInstruction(const char *name, int sign, Chunk *chunk,
+                           int offset) {
+  uint16_t jump = (uint16_t)(chunk->code[offset + 1] << 8);
+  jump |= (uint16_t)chunk->code[offset + 2];
   printf("%-16s %4d -> %d\n", name, offset, offset + 3 + sign * jump);
   return offset + 3;
 }
