dfc6bde12fae1846d9045c4a12c5878421e28344
shinokun <168421748+hypatia-tile@users.noreply.github.com>
2026-07-31 09:04:19 +0900

    format: use nixfmt (#42)

--- flake.nix
+++ flake.nix
@@ -5,47 +5,54 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-    pkgsFor = system:
-      import nixpkgs {
-        inherit system;
-      };
-  in {
-    devShells = forAllSystems (
-      system: let
-        pkgs = pkgsFor system;
-        isDarwin = pkgs.stdenv.isDarwin;
-        isLinux = pkgs.stdenv.isLinux;
-      in {
-        default = pkgs.mkShell {
-          packages = with pkgs;
-            [
-              clang
-              clang-tools
-              gnumake
-              pkg-config
-              bear
-            ]
-            ++ pkgs.lib.optionals isLinux [
-              glibc.dev
-            ]
-            ++ pkgs.lib.optionals (!isDarwin) [
-              lldb
-            ];
+      pkgsFor =
+        system:
+        import nixpkgs {
+          inherit system;
         };
-      }
-    );
-  };
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgsFor system;
+          isDarwin = pkgs.stdenv.isDarwin;
+          isLinux = pkgs.stdenv.isLinux;
+        in
+        {
+          default = pkgs.mkShell {
+            packages =
+              with pkgs;
+              [
+                clang
+                clang-tools
+                gnumake
+                pkg-config
+                bear
+              ]
+              ++ pkgs.lib.optionals isLinux [
+                glibc.dev
+              ]
+              ++ pkgs.lib.optionals (!isDarwin) [
+                lldb
+              ];
+          };
+        }
+      );
+    };
 }
