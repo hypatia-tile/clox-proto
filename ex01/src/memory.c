813dd63c25f947e6b0b54608ce051476ec4ee624
shinokun <hypatia.tile02021@gmail.com>
2026-09-02 07:57:01 +0900

    feat(25.3.1): Upvalues in closures
    Add fields `upvalues` and `upvalueCount` to ObjClosure.
    Associate the upvalue's memory behavior with ObjClosure.

--- ex01/src/memory.c
+++ ex01/src/memory.c
@@ -21,6 +21,8 @@ void *reallocate(void *pointer, size_t oldSize, size_t newSize) {
 static void freeObject(Obj *object) {
   switch (object->type) {
   case OBJ_CLOSURE: {
+    ObjClosure *closure = (ObjClosure *)object;
+    FREE_ARRAY(ObjUpvalue *, closure->upvalues, closure->upvalueCount);
     FREE(ObjClosure, object);
   }
   case OBJ_FUNCTION: {
