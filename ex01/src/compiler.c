28d9d6fca52521f36612d9eb87663e6bc12f23f2
shinokun <hypatia.tile02021@gmail.com>
2026-09-02 21:34:50 +0900

    feat(25.4.2): Closing upvalues
    Emit OP_CLOSE_UPVALUE when captured local variable is poped.

--- ex01/src/compiler.c
+++ ex01/src/compiler.c
@@ -60,6 +60,7 @@ typedef struct {
 typedef struct {
   Token name;
   int depth;
+  bool isCaptured;
 } Local;
 
 typedef struct {
@@ -217,6 +218,7 @@ static void initCompiler(Compiler *compiler, FunctionType type) {
 
   Local *local = &current->locals[current->localCount++];
   local->depth = 0;
+  local->isCaptured = false;
   local->name.start = "";
   local->name.length = 0;
 }
@@ -242,7 +244,12 @@ static void endScope() {
 
   while (current->localCount > 0 &&
          current->locals[current->localCount - 1].depth > current->scopeDepth) {
-    emitByte(OP_POP);
+    if (current->locals[current->localCount - 1].isCaptured) {
+      emitByte(OP_CLOSE_UPVALUE);
+    } else {
+      emitByte(OP_POP);
+    }
+
     current->localCount--;
   }
 }
@@ -373,10 +380,12 @@ static int addUpvalue(Compiler *compiler, uint8_t index, bool isLocal) {
 }
 
 static int resolveUpvalue(Compiler *compiler, Token *name) {
-  if (compiler->enclosing == NULL) return -1;
+  if (compiler->enclosing == NULL)
+    return -1;
 
   int local = resolveLocal(compiler->enclosing, name);
   if (local != -1) {
+    compiler->enclosing->locals[local].isCaptured = true;
     return addUpvalue(compiler, (uint8_t)local, true);
   }
 
@@ -396,6 +405,7 @@ static void addLocal(Token name) {
   Local *local = &current->locals[current->localCount++];
   local->name = name;
   local->depth = -1;
+  local->isCaptured = false;
 }
 
 static void declareVariable() {
